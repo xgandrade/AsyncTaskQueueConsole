0695a3628f2af36a5485da46b9a5c9e990bd5f8f
Guilherme Andrade <guilherme_gsa@live.com>
2024-09-27 21:47:29 -0300

    Commit initial

---
+++ AsyncTaskQueueConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using AsyncTaskQueueConsole.Domain;
+using AsyncTaskQueueConsole.Interfaces;
+using AsyncTaskQueueConsole.Services;
+using System;
+using System.Collections.Generic;
+
+namespace AsyncTaskQueueConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IContabilizadorService contabilizadorService = new ContabilizadorService();
+            List<Contabilizador> listaDeTarefas = GerarTarefas(10);
+            await contabilizadorService.ProcessarTarefasAsync(listaDeTarefas);
+        }
+
+        static List<Contabilizador> GerarTarefas(int quantidade)
+        {
+            Random random = new();
+            List<Contabilizador> tarefas = new();
+
+            for (int i = 1; i <= quantidade; i++)
+            {
+                int tempoProcessamento = random.Next(1, 6); 
+                tarefas.Add(new Contabilizador(i, tempoProcessamento));
+            }
+
+            return tarefas;
+        }
+    }
+}
